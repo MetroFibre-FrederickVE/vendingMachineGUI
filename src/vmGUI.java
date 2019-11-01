2622216037b9262ed77c6a806e39d7d38f07b4e9
FJvanEeden <fredz.eeden@gmail.com>
2019-11-01 22:05:09 +0200

    Added comments and fixed readability.

--- src/vmGUI.java
+++ src/vmGUI.java
@@ -1,11 +1,10 @@
+// Library Imports.
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import java.awt.GridLayout;
 import java.awt.Font;
 import javax.swing.JButton;
@@ -14,7 +13,6 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -27,8 +25,12 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.text.DecimalFormat;
 
+/* Main Class that extends the JFrame and creates the 
+   elements on the frame. */
 public class vmGUI extends JFrame {
 
+	
+	// Assign variable names frame elements. 
 	private JPanel contentPane;
 	private JTextField moneyInTF;
 	private JTextField moneyOutTF;
@@ -40,13 +42,16 @@ public class vmGUI extends JFrame {
 	private JTextField energyDrinkTF;
 	private JTextField totalTF;
 
+	// Creates new format for float values to round of to 2 decimal places.
 	private static DecimalFormat df = new DecimalFormat("0.00");
+	
 	
 	/**
 	 * Launch the application.
 	 * @throws IOException 
 	 * @throws FileNotFoundException 
 	 */
+	// Initiates main method of class.
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -60,7 +65,6 @@ public class vmGUI extends JFrame {
 		});
 	}
 		
-
 	/**
 	 * Create the frame.
 	 * @throws IOException 
@@ -68,6 +72,7 @@ public class vmGUI extends JFrame {
 	 */
 	public vmGUI() throws FileNotFoundException, IOException {
 		
+		// Creates and customizes the JFrame's position values.
 		setResizable(false);
 		setTitle("Vending Machine");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -77,15 +82,21 @@ public class vmGUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
 		
+		
+		// Reads data from CSV to a list.
 		List<List<String>> records = new ArrayList<>();
+		// Accesses file from path.
 		try (BufferedReader br = new BufferedReader(new FileReader("data/stockList.csv"))) {
 		    String line;
 		    while ((line = br.readLine()) != null) {
+		    	// Splits data at occurance of ","
 		        String[] values = line.split(",");
 		        records.add(Arrays.asList(values));
 		    }
 		}
 		
+		
+		// Creates integer array to act as click counter
 		int[] sdClick = {0};
 		int[] edClick = {0};
 		int[] scClick = {0};
@@ -93,20 +104,20 @@ public class vmGUI extends JFrame {
 		int[] caClick = {0};
 		int[] icClick = {0};
 		
-		
-		List<String> sdData = records.get(0);
+		// Creates List to hold records found from CSV file
+		List<String> sdData = records.get(0); // Number represents line number within CSV
 		List<String> edData = records.get(1);
 		List<String> scData = records.get(2);
 		List<String> lcData = records.get(3);
 		List<String> icData = records.get(4);
 		List<String> caData = records.get(5);
 		
-		
+		// Creates main panel containing.
 		JPanel panel = new JPanel();
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		// Panel containing item buttons
+		// Creates panel containing item buttons.
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "Please select your items...", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_1.setBounds(10, 70, 524, 212);
@@ -188,6 +199,7 @@ public class vmGUI extends JFrame {
 		lblNewLabel_2.setBounds(364, 402, 75, 20);
 		panel.add(lblNewLabel_2);
 		
+		
 		// Validation
 		moneyInTF = new JTextField();
 		moneyInTF.addKeyListener(new KeyAdapter() {
@@ -218,111 +230,116 @@ public class vmGUI extends JFrame {
 		purchaseTA.setLineWrap(true);
 		purchaseTA.setFont(new Font("Arial", Font.BOLD, 13));
 		purchaseTA.setWrapStyleWord(true);
-		purchaseTA.setText("         Selected items for Purchase : \r\n\r\n");
+		purchaseTA.setText("   Selected items for Purchase : \r\n\r\n");
 		purchaseTA.setEditable(false);
 		purchaseTA.setBounds(42, 292, 295, 161);
 		panel.add(purchaseTA);
 		
+		// Creates button and adds event handler
 		JButton sdBT = new JButton("Soft Drinks");
 		sdBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float sdTemp = Float.parseFloat(sdData.get(1));
 				sdClick[0] += sdTemp;
-				
+				// Then displays data in text area.
 				purchaseTA.append("Item : " + sdData.get(0) + " - Price : R " + sdData.get(1) + "\n");
-				
 			}
 		});
 		sdBT.setBounds(20, 22, 136, 42);
 		panel_1.add(sdBT);
 		
+		// Creates button and adds event handler
 		JButton scBT = new JButton("Small Chips");
 		scBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float scTemp = Float.parseFloat(scData.get(1));
 				scClick[0] += scTemp;
-				
+				// Then displays data in text area.
 				purchaseTA.append("Item : " + scData.get(0) + " - Price : R " + scData.get(1) + "\n");
-				
 			}
 		});
 		scBT.setBounds(196, 22, 136, 42);
 		panel_1.add(scBT);
 		
+		// Creates button and adds event handler
 		JButton caBT = new JButton("Candy's");
 		caBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float caTemp = Float.parseFloat(caData.get(1));
 				caClick[0] += caTemp;
-				
-				purchaseTA.append("Item : " + caData.get(0) + " - Price : R " + caData.get(1) + "\n");
-				
+				// Then displays data in text area.
+				purchaseTA.append("Item : " + caData.get(0) + " - Price : R " + caData.get(1) + "\n");	
 			}
 		});
 		caBT.setBounds(378, 22, 136, 42);
 		panel_1.add(caBT);
 		
+		// Creates button and adds event handler
 		JButton edBT = new JButton("Energy Drinks");
 		edBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float edTemp = Float.parseFloat(edData.get(1));
 				edClick[0] += edTemp;
-				
+				// Then displays data in text area.
 				purchaseTA.append("Item : " + edData.get(0) + " - Price : R " + edData.get(1) + "\n");
-				
 			}
 		});
 		edBT.setBounds(20, 116, 136, 42);
 		panel_1.add(edBT);
 		
+		// Creates button and adds event handler
 		JButton lcBT = new JButton("Large Chips");
 		lcBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float lcTemp = Float.parseFloat(lcData.get(1));
 				lcClick[0] += lcTemp;
-				
-				purchaseTA.append("Item : " + lcData.get(0) + " - Price : R " + lcData.get(1) + "\n");
-				
+				// Then displays data in text area.
+				purchaseTA.append("Item : " + lcData.get(0) + " - Price : R " + lcData.get(1) + "\n");	
 			}
 		});
 		lcBT.setBounds(196, 116, 136, 42);
 		panel_1.add(lcBT);
 		
+		// Creates button and adds event handler
 		JButton icBT = new JButton("Ice-cream");
 		icBT.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				// Adds the amount of selected items on every button press.
 				float icTemp = Float.parseFloat(icData.get(1));
 				icClick[0] += icTemp;
-				
+				// Then displays data in text area.
 				purchaseTA.append("Item : " + icData.get(0) + " - Price : R " + icData.get(1) + "\n");
-				
 			}
 		});
 		icBT.setBounds(378, 116, 136, 42);
 		panel_1.add(icBT);
 		
+		
 		// Action listener for "Purchase" button.
-		// Button press checks money amount against purchase price.						
+		// Displays thank you message and and exits program.					
 		JButton purchaseBTN = new JButton("Pay");
 		purchaseBTN.setVisible(false);
 		purchaseBTN.addActionListener(new ActionListener() {
-			
 			public void actionPerformed(ActionEvent e) {
 				
 				JOptionPane.showMessageDialog(null, "Thank you for your purchase & please call again.\n Press Ok to exit the program.");
 				System.exit(0);
-				
 			}
-
 		});
 		purchaseBTN.setBounds(73, 465, 89, 35);
 		panel.add(purchaseBTN);
+		
 		
 		// Action listener for "Cancel" button.
 		// Button press closes application window.
@@ -331,20 +348,22 @@ public class vmGUI extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				System.exit(0);
-				
 			}
 		});
 		cancelBTN.setBounds(364, 465, 89, 35);
 		panel.add(cancelBTN);
 		
-        // Checkout button
-        JButton btnCheckout = new JButton("Check-out");
-        btnCheckout.addActionListener(new ActionListener() {
+		
+        // Creates Checkout button and adds event handler.
+        JButton checkoutBTN = new JButton("Check-out");
+        checkoutBTN.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         		
+        		// Sets visibility of buttons on click.
         		purchaseBTN.setVisible(true);
-        		btnCheckout.setVisible(false);
+        		checkoutBTN.setVisible(false);
         		
+        		// Sets variable values equal to monetary value of item form CSV.
         		float softDrinkPrice = sdClick[0];
         		float energyDrinkPrice = edClick[0];
         		float smlChipsPrice = scClick[0];
@@ -352,6 +371,7 @@ public class vmGUI extends JFrame {
         		float candysPrice = caClick[0];
         		float iceCreamPrice = icClick[0];
         		
+        		// Adds all item values together.
         		float tempTotal = softDrinkPrice 
         				+ energyDrinkPrice
         				+ smlChipsPrice
@@ -362,46 +382,53 @@ public class vmGUI extends JFrame {
 				float total = 0 ;
 				float moneyIn = (float) 0.00;
 				
+				// Checks if user entered any money.
+				// If not, a warning message is displayed.
 				if (moneyInTF.getText().isEmpty()) {
 					JOptionPane.showMessageDialog(rootPane, "Please enter your money before checking out.");
 				} else {
+					// Else, sets variable value equal to amount entered.
 					moneyIn = Float.parseFloat(moneyInTF.getText());
 				}
 				
-				
+				// Checks is total amount of items selected is larger than 0.
 				if (tempTotal > 0) {
-					totalTF.setText(Double.toString(tempTotal));
+					// Calls formatter to set decimal value after rounding.
+					df.setRoundingMode(RoundingMode.HALF_EVEN);
+					totalTF.setText(df.format(tempTotal));
 					total = tempTotal ;
 				} else {
+					// Else, an error message is displayed.
 					JOptionPane.showMessageDialog(rootPane, "Error");
 				}
 				
+				// Checks if user entered enough money for purchase.
 				if (moneyIn < 0 || moneyIn < total) {
+					// Displays error message and resets "Checkout" process.
 					JOptionPane.showMessageDialog(rootPane, "Not enough money.");
 					purchaseBTN.setVisible(false);
-					btnCheckout.setVisible(true);
+					checkoutBTN.setVisible(true);
 				} else {
+					// Else, calculates users change amount and displays.
 					Double input = (double) (moneyIn - total);
-					df.setRoundingMode(RoundingMode.UP);
 					moneyOutTF.setText(df.format(input));
 				}
-				
-				
-        		
         	}
         });
-        btnCheckout.setVisible(true);
-        btnCheckout.setBounds(52, 465, 120, 35);
-        panel.add(btnCheckout);
+        checkoutBTN.setVisible(true);
+        checkoutBTN.setBounds(52, 465, 120, 35);
+        panel.add(checkoutBTN);
 		
+        
 		// Action listener for "Clear" button.
-		// Button press clears all selected radio buttons and text fields.
+		// Button press clears all selected items and text fields.
 		JButton clearBTN = new JButton("Restart");
 		clearBTN.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				purchaseTA.setText("Selected items for Purchase : \r\n\r\n");
 				
+				// Resets all Counter array values.
 				sdClick[0] = 0;
 				edClick[0] = 0;
 				scClick[0] = 0;
@@ -409,26 +436,29 @@ public class vmGUI extends JFrame {
 				caClick[0] = 0;
 				icClick[0] = 0;
 				
+				// Displays blank values to text fields.
 				moneyInTF.setText(" ");
 				moneyOutTF.setText(" ");
 				totalTF.setText(" ");
 				
+				// Changes visibility of buttons and sets editability.
         		purchaseBTN.setVisible(false);
-        		btnCheckout.setVisible(true);
-        		
+        		checkoutBTN.setVisible(true);
         		moneyInTF.setEditable(true);
 			}
 		});
 		clearBTN.setBounds(222, 465, 89, 35);
 		panel.add(clearBTN);
 		
-        // Change text field value
+		
+        // Change text field values to money values from CSV
         softDrinkTF.setText(sdData.get(1));
         energyDrinkTF.setText(edData.get(1));
         smlChipsTF.setText(scData.get(1));
         lrgChipsTF.setText(lcData.get(1));
         candyTF.setText(caData.get(1));
         iceCreamTF.setText(icData.get(1));
+        
         
         totalTF = new JTextField();
         totalTF.setEditable(false);
